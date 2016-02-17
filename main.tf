153669b76a6c0fb0e64da0ba4d0274632f81dd55
Marcin Kulisz <marcin@bashton.com>
2016-02-17 11:10:42 +0000

    Initial commit, all available TF options for +
    aws_ebs_volume resource are defined but snapshot_id & kms_key_id are not used
    for now

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_ebs_volume" "ebs" {
+  availability_zone = "${var.name}"
+  encrypted         = "${var.encrypted}"
+  iops              = "${var.iops}"
+  size              = "${var.size}"
+#  snapshot_id       = "${var.snapshot_id}"
+  type              = "${var.type}"
+#  kms_key_id        = "${var.kms_key_id}"
+  tags = {
+    Name    = "${var.name_tag}"
+    Envname = "${var.envname_tag}"
+    Service = "${var.service_tag}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "availability_zone" { }
+variable "encrypted" {default = "false"}
+variable "iops" {default = 0}
+variable "size" { }
+#variable "snapshot_id" { }
+variable "type" {default = "gp2"}
+#variable "kms_key_id" { }
+variable "name_tag" { }
+variable "envname_tag" { }
+variable "service_tag" { }
